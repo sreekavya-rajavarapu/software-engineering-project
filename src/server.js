3fdc59947e845e035e41bca3e18dacf4bb2685fa
sreekavya-rajavarapu <sreekavya02@gmail.com>
2019-02-24 19:39:15 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,117 @@
+import sirv from 'sirv';
+const express = require('express')
+import compression from 'compression';
+import * as sapper from '../__sapper__/server.js';
+const app = express()
+const port = 3000
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var flash = require('express-flash');
+const ejs = require('ejs')
+var session = require('express-session');
+// file store for SESSIONS
+var MemoryStore = require('session-memory-store')(session);
+
+
+app.use(
+	session({
+  	secret: 'csu-enrollment-project',
+		store: new MemoryStore(),
+  	resave: false,
+  	saveUninitialized: false
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+// template engine and flash support
+app.use(flash());
+app.engine('.ejs', ejs.renderFile);
+
+passport.use(new Strategy(
+  function(username, password, done) {
+    if (username == 'admin' && password == 'admin') {
+			return done(null, {'id': 'admin', 'password' : 'admin'});
+		} else {
+			return done(null, false);
+		}
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	if(id == 'admin') {
+		done(null, {'id': 'admin', 'password' : 'admin'});
+	}
+});
+
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(fileUpload());
+
+
+
+// intercept Basic auth in case the api is being used without client cookies.
+app.all('/api/*', (req,res,next) => {
+	if(req.isAuthenticated()){
+    next();
+  } else {
+    console.log(new Date() + ` not yet authenticated for: ${req.path}`);
+    passport.authenticate('local', function(err, user) {
+      req.login({ id: 'admin', password: 'admin' },{ session: false }, function(err) {
+          if(err) {return next(err)}
+          next()
+      })
+    })(req, res, next)
+  }
+});
+
+// render login page with any flash error messages
+app.get('/login', (req,res) => {
+	let flashMsg = req.flash();
+	res.render(path.join(process.cwd(), 'src', 'components','login.ejs'),{flash: flashMsg});
+});
+
+// after passport serializes user object
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+		console.log("Redirecting");
+    res.redirect('/');
+  });
+
+
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  }
+);
+
+
+// main route, handle sapper stuff after checking authentication.
+// app.use(check, sapper({ manifest }));
+
+app.use(
+	function(req, res, next){
+			if(req.isAuthenticated()){
+				next();
+			} else {
+				res.redirect("/login");
+			}
+		},
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
