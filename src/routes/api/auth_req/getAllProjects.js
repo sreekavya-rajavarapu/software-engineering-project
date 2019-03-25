2baef3d8b3b6a41c5f4db25622b938e7ebadf003
sreekavya-rajavarapu <sreekavya02@gmail.com>
2019-03-24 21:27:40 -0400

    add dropzone file upload feature

--- src/routes/api/auth_req/getAllProjects.js
+++ src/routes/api/auth_req/getAllProjects.js
@@ -5,7 +5,6 @@ export function get(req, res, next) {
   // get all projects from database
   let response = []
   db.Project.findAll().then((projects) => {
-    console.log(projects);
     _.each(projects, (project) => {
       let temp = {}
       temp['title'] = project.title;
