1fdf5860906be7cc6bf889beaa1b9866b5c8b7bf
Sree Kavya <sreekavya02@gmail.com>
2019-05-10 10:44:30 -0400

    user can only add 5 projects

--- src/routes/api/auth_req/addNewEnrollment.js
+++ src/routes/api/auth_req/addNewEnrollment.js
@@ -7,18 +7,29 @@ export function post(req, res, next) {
   const nDate = new Date().toLocaleString('en-US', {
     timeZone: 'America/New_York'
   });
-  console.log(nDate);
+  // check if user has already enrolled for 5 projects if so
   db.Enrollment.sync().then(() => {
-    db.User.sync().then(() => {
-      db.User.findOne({where: {csuid: userID}}).then((user) => {
-        db.Project.findOne({where: {project_id: projectID}}).then((project) => {
-          let insertRow = _.extend({date_of_enrollment: nDate},{user_id: user.csuid}, {project_id: project.project_id})
-          console.log(insertRow);
-          db.Enrollment.create(insertRow).then((enrollment) => {
-            res.json(enrollment.id);
+    db.Enrollment.findAll({where: {user_id: {[db.Sequelize.Op.like] : userID}}}).then((enrollments) => {
+      if (enrollments.length >= 5) {
+        res.json("Already enrolled for 5 projects. Delete selection to continue")
+        return
+      } else {
+        db.Enrollment.sync().then(() => {
+          db.User.sync().then(() => {
+            db.User.findOne({where: {csuid: userID}}).then((user) => {
+              db.Project.findOne({where: {project_id: projectID}}).then((project) => {
+                let insertRow = _.extend({date_of_enrollment: nDate},{user_id: user.csuid}, {project_id: project.project_id})
+                console.log(insertRow);
+                db.Enrollment.create(insertRow).then((enrollment) => {
+                  res.json(enrollment.id);
+                })
+              })
+            })
           })
         })
-      })
+      }
     })
   })
+
+
 }
